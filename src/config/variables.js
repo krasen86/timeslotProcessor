a72c53db369ecd539af2dd40e340c6becf449f39
jeskanen <jeskanen@student.chalmers.se>
2020-11-23 15:06:09 +0100

    Revert "Add codebase, create mqtt broker connection, subscribe to dentists,..."
    This reverts commit 961c79a47cf853a72abdb45dd601894824503c77

--- src/config/variables.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    URL: 'mqtt://127.0.0.1', // localhost
-    DENTIST_TOPIC: 'dentists'
-};
-
-
--- src/dentist-watcher/mqttConnector.js
+++
@@ -1,4 +0,0 @@
-const mqtt = require('mqtt');
-const Variables = require("../config/variables");
-
-module.exports.MQTT = mqtt.connect(Variables.URL);
