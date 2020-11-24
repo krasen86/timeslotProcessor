1dd3dcb2275ebfc1c6f0321434ea627440e159ec
tammen <gustammjo@student.gu.se>
2020-11-24 17:00:50 +0100

    #1 Printing hardcoded availability
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>

--- src/availability-handler/availabilityController.js
+++ src/availability-handler/availabilityController.js
@@ -3,7 +3,28 @@ class AvailabilityController {
     }
     processMessage(message) {
         const buffer = message.toString('utf-8');
-        console.log(JSON.parse(buffer));
+        //console.log(buffer)
+        this.splitMessage(buffer);
+    }
+
+    splitMessage(message) {
+        const clinics = JSON.parse(message).dentists
+        //console.log(clinics)
+        for (var i = 0; i < clinics.length ; i++ ) {
+
+            //add availability Array to each clinic
+            clinics[i].availability = []
+
+
+            clinics[i].availability.push({"2020-10-23": [{"08.00 - 08.30": 2}]})
+        }
+
+
+
+
+
+        console.log(JSON.stringify(clinics,null, " "))
+
     }
 
 }
