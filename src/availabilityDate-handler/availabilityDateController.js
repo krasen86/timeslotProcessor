26a7e6c4298ea811860d1e957ee480220939734f
chrtsa <chrtsa@student.chalmers.se>
2020-12-16 16:46:29 +0200

    #10 Work in progress for clinics' availability per date functionality
     Co-Authored-By: Johann Tammen <tammen@student.chalmers.se>

--- src/availabilityDate-handler/availabilityDateController.js
+++ src/availabilityDate-handler/availabilityDateController.js
@@ -8,24 +8,55 @@ class AvailabilityDateController {
 
     initiateAvailabilityPerDay() {
 
-        this.publisher.publishAvailabilityForDate('2020-12-14', 'EINS IM CHAT')
-        this.getAvailabilityForClinicForDate(1, '2020-12-15')
+        const availabilityDir = './availability-data/';
+        let clinicsNumber = 3;
+        // fs.readdir(availabilityDir, (err, files) => {
+        //     return files.length
+        // })
+        let response = this.getAvailabilityForAllClinicsForDate(clinicsNumber, '2020-12-15');
 
+        response.then(res => {
+            console.log('final response: ' + res);
+            this.publisher.publishAvailabilityForDate('2020-12-15', res);
+        })
+
+    }
+
+    getAvailabilityForAllClinicsForDate(clinicsNumber, date) {
+        return new Promise ((resolve) => {
+            let response = '';
+            for (let i = 1; i <= clinicsNumber; i++) {
+                let isAvailable =  this.getAvailabilityForClinicForDate(i, '2020-12-15');
+                isAvailable.then(res => {
+                    response += i + ' ' + res + ' '
+                    console.log(response)
+                }).catch(err => {
+                    console.log(err);
+                })
+            }
+            resolve(response)
+        })
     }
 
     getAvailabilityForClinicForDate(clinicId, date) {
         let fileName = './availability-data/availability-' + clinicId +'.json'
         try {
-            fs.readFile (fileName, (err, data) => {
-                let availability = JSON.parse(data).availability;
-                let timeslotArray = availability[0][date]
+            return new Promise((resolve) => {
+                fs.readFile(fileName, (err, data) => {
+                    let availability = JSON.parse(data).availability;
+                    let timeslotArray = availability[0][date];
 
-                console.log(availability)
 
+                    let response = timeslotArray.some((timeSlot, index) => {
+                        return Object.values(timeslotArray[index]) >= 1;
+                    })
+
+                    console.log(response);
+                    resolve(response.toString());
+                })
             })
-
         } catch(err) {
-            console.error(err)
+            console.error(err);
         }
     }
 }
