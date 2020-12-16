346440e055745e03c66ebfd9a10d756f1a1da40c
chrtsa <chrtsa@student.chalmers.se>
2020-12-16 18:02:51 +0200

    #10 Work in progress availability for all clinics for date functionality
     Co-Authored-By: Johann Tammen <tammen@student.chalmers.se>

--- src/availabilityDate-handler/availabilityDateController.js
+++ src/availabilityDate-handler/availabilityDateController.js
@@ -10,31 +10,42 @@ class AvailabilityDateController {
 
         const availabilityDir = './availability-data/';
         let clinicsNumber = 3;
+        let date = '2020-12-17';
         // fs.readdir(availabilityDir, (err, files) => {
         //     return files.length
         // })
-        let response = this.getAvailabilityForAllClinicsForDate(clinicsNumber, '2020-12-15');
+
+        let response = this.getAvailabilityForAllClinicsForDate(clinicsNumber, date);
 
         response.then(res => {
-            console.log('final response: ' + res);
-            this.publisher.publishAvailabilityForDate('2020-12-15', res);
+            console.log('final response: ' + JSON.stringify(res));
+            this.publisher.publishAvailabilityForDate(date, JSON.stringify(res));
         })
 
     }
 
+    /*availabilityDate/2020-12-10
+        {
+        “Id1”: true,
+        “Id2”: true,av
+        “Id3”: false
+        }
+    */
+
     getAvailabilityForAllClinicsForDate(clinicsNumber, date) {
         return new Promise ((resolve) => {
-            let response = '';
+            let response = {};
             for (let i = 1; i <= clinicsNumber; i++) {
-                let isAvailable =  this.getAvailabilityForClinicForDate(i, '2020-12-15');
+                let isAvailable =  this.getAvailabilityForClinicForDate(i, date);
                 isAvailable.then(res => {
-                    response += i + ' ' + res + ' '
-                    console.log(response)
+                    response["Id"+ i] = res;
+                    console.log(response);
                 }).catch(err => {
                     console.log(err);
                 })
             }
-            resolve(response)
+
+            resolve(response);
         })
     }
 
@@ -43,15 +54,14 @@ class AvailabilityDateController {
         try {
             return new Promise((resolve) => {
                 fs.readFile(fileName, (err, data) => {
-                    let availability = JSON.parse(data).availability;
-                    let timeslotArray = availability[0][date];
+                    let availabilityArray = JSON.parse(data).availability;
 
-
-                    let response = timeslotArray.some((timeSlot, index) => {
-                        return Object.values(timeslotArray[index]) >= 1;
+                    let dateObject = availabilityArray.find(obj => obj.date === date);
+                    let response = dateObject.timeslots.some((timeSlot) => {
+                        return timeSlot.availableDentists >= 1;
                     })
 
-                    console.log(response);
+                    console.log('getAvailabilityForClinicForDate: ' + response);
                     resolve(response.toString());
                 })
             })
