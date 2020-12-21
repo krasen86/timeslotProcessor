6e81846739e8ef73bc8a693e7b78d74f623f5897
chrtsa <chrtsa@student.chalmers.se>
2020-12-21 13:04:22 +0200

    Hotfix for merge issue in timeSlotCreator.js
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>

--- src/timeslot-handler/timeSlotCreator.js
+++ src/timeslot-handler/timeSlotCreator.js
@@ -34,20 +34,20 @@ class TimeSlotCreator {
 
         //parse hour and minutes for opening
 
-            let startHour = parseInt(start.split(':')[0]);
-            let startMinute = '00';
+        let startHour = parseInt(start.split(':')[0]);
+        let startMinute = '00';
 
         //parse hour for closing to calculate amount of timeslots
 
-            let closeTime = close.split(':')[0];
-            let timeSlots = (closeTime-startHour)*2;
+        let closeTime = close.split(':')[0];
+        let timeSlots = (closeTime-startHour)*2;
 
         //assign temp variables to manipulate within for loop
 
-            let tempHour = startHour;
-            let tempMinute = startMinute;
-            let startHalfHour = start.split(':')[1]
-            let closeHalfHour = close.split(':')[1]
+        let tempHour = startHour;
+        let tempMinute = startMinute;
+        let startHalfHour = start.split(':')[1]
+        let closeHalfHour = close.split(':')[1]
 
         // When starting hour equals ending hour, e.g. 9:00-9:30 push to timeslotArray and return its value
         // In case closing hour is not half an hour later this does not get executed (e.g. getting from
@@ -68,47 +68,47 @@ class TimeSlotCreator {
         //for loop pushes to timearray on first iteration with only adding +30 minutes to endtime of timeslot
         //iterate as many times as there are supposed to be timeslots
 
-            for (var i=0; i<timeSlots; i++){
-                if(i === 0) {
-                    timeslotArray.push({
-                        "time": startHour + ':' + tempMinute + ' - ' + (startHour) + ':' + (parseInt(tempMinute + 30)),
-                        "availableDentists": clinic.dentists
-                    });
-                   // timeArray.push({[startHour + ':' + tempMinute + ' - ' + (startHour) + ':' + (parseInt(tempMinute+30))]: clinic.dentists})
-                } else {
-                    tempMinute = parseInt(tempMinute) + 30;
-        //check if hour needs to be added
-                    if (tempMinute === 60) {
-                        tempMinute = '00';
-                        tempHour += 1;
-                    }
-        //create ending hours and minutes for the timeslots, always 30 minutes
-                    var endHour = tempHour;
-                    var endMinute = parseInt(tempMinute) + 30;
-
-                    if (endMinute === 60) {
-                        endMinute = '00';
-                        endHour += 1;
-                    }
-        //push the timeslot for every iteration to the array, with the amount of dentists the clinic has
-                    timeslotArray.push({
-                        "time": tempHour + ':' + tempMinute + ' - ' + endHour + ':' + endMinute,
-                        "availableDentists": clinic.dentists
-                    });
-                    //timeArray.push({[tempHour + ':' + tempMinute + ' - ' + endHour + ':' + endMinute]: clinic.dentists})
-                }
-            }
-            if (startHalfHour === '30') {
-                timeslotArray.splice(0,1)
-            }
-            if (closeHalfHour === '30'){
-                tempHour += 1
-                tempMinute = '00'
+        for (var i=0; i<timeSlots; i++){
+            if(i === 0) {
                 timeslotArray.push({
-                    "time": tempHour + ':' + tempMinute + ' - ' + endHour + ':' + (parseInt(endMinute + 30)),
+                    "time": startHour + ':' + tempMinute + ' - ' + (startHour) + ':' + (parseInt(tempMinute + 30)),
                     "availableDentists": clinic.dentists
-                })
+                });
+                // timeArray.push({[startHour + ':' + tempMinute + ' - ' + (startHour) + ':' + (parseInt(tempMinute+30))]: clinic.dentists})
+            } else {
+                tempMinute = parseInt(tempMinute) + 30;
+                //check if hour needs to be added
+                if (tempMinute === 60) {
+                    tempMinute = '00';
+                    tempHour += 1;
+                }
+                //create ending hours and minutes for the timeslots, always 30 minutes
+                var endHour = tempHour;
+                var endMinute = parseInt(tempMinute) + 30;
+
+                if (endMinute === 60) {
+                    endMinute = '00';
+                    endHour += 1;
+                }
+                //push the timeslot for every iteration to the array, with the amount of dentists the clinic has
+                timeslotArray.push({
+                    "time": tempHour + ':' + tempMinute + ' - ' + endHour + ':' + endMinute,
+                    "availableDentists": clinic.dentists
+                });
+                //timeArray.push({[tempHour + ':' + tempMinute + ' - ' + endHour + ':' + endMinute]: clinic.dentists})
             }
+        }
+        if (startHalfHour === '30') {
+            timeslotArray.splice(0,1)
+        }
+        if (closeHalfHour === '30'){
+            tempHour += 1
+            tempMinute = '00'
+            timeslotArray.push({
+                "time": tempHour + ':' + tempMinute + ' - ' + endHour + ':' + (parseInt(endMinute + 30)),
+                "availableDentists": clinic.dentists
+            })
+        }
         return timeslotArray;
 
     }
@@ -142,8 +142,6 @@ class TimeSlotCreator {
     }
 
     updateTimeslots(dentist, day, newHours) {
-        const storageController = new StorageController();
-
         // goes through availability array before any changes are made
         const clinicDataStore = new ClinicDataStore();
         for(let i = 0; i<dentist.availability.length; i++) {
